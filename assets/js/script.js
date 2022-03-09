8d193e2ca48f268d6fdf3944d4506af85a1e7335
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-03-08 19:52:08 -0600

    created HTML and CSS to show the area the quiz will be taken

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// this is a coding quiz.
+
+//click a button that will start the timer as well as show the first question.
+
+//create a function that will display a question.
+
+//create a function that will move on to the next question once an answer has been given.
+//if a question is answered incorrectly, remove time from the clock.
+
+//repeat the process of displaying a question and moving to the next question when answered.
+
+//end quiz when timer runs out or all questions are answered.
+
+//create a function that will display the number of questions answered correctly.
+
+//find a way to save the high scores.
+//find a way to enter initials where the scores are being displayed.
+//create a button to clear high scores.
+
+//ask to play the game again.
+
