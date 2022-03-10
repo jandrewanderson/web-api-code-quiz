8568273a630ff579b1ad4c89a7406e53ff5ae15f
Joseph Anderson <jandrewanderson1995@gmail.com>
2022-03-09 18:32:42 -0600

    added all creatElements and made comments to organize js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,97 @@
 // this is a coding quiz.
 
 //global variables
-var startBtn = document.querySelector("#startBtn");
-var timerEl = document.querySelector("#countdown");
-var timer = document.querySelector("#timer");
+
+//vars for the start button
+    //this is selecting the start quiz button at the bottom
+        var startBtn = document.querySelector("#startBtn");
+
+//vars for the timer
+    //this var is selecting the box directly to the right of "Timer:"
+        var timerEl = document.querySelector("#countdown");
+    //this var is selecting the box that says timer
+        var timer = document.querySelector("#timer");
+
+//added Elements to section-body
+    /*  <section class="section-body">
+            <div id="questions">
+                <div> (createQuestions)
+                    <h3>
+                    <h4>
+                    <ol>
+                        <li> *4
+    */
+        //vars for the body where the questions will be displayed
+        //var to select the contents in the main box where the questions will be displayed.
+        var questionBox = document.getElementById("questions");
+        //this var creates a div, which will be inside div #questions
+        var createQuestion = document.createElement("div");
+    //inside the div
+        //creates h3 inside div
+        var createQuestionH3 = document.createElement("h3");
+        //creates h4 inside div. This will display the actual question
+        var createQuestionH4 = document.createElement("h4");
+        //this creates an ol inside the createQuestion div.
+        var answerList = document.createElement("ol");
+        //this creates li inside the ol that will display the answer options to the question
+        var answer1 = document.createElement("li");
+        var answer2 = document.createElement("li");
+        var answer3 = document.createElement("li");
+        var answer4 = document.createElement("li");
+
+
+    //all appends 
+
+        questionBox.appendChild(createQuestion);
+        createQuestion.appendChild(createQuestionH3);
+        createQuestion.appendChild(createQuestionH4);
+        createQuestion.appendChild(answerList);
+        answerList.appendChild(answer1);
+        answerList.appendChild(answer2);
+        answerList.appendChild(answer3);
+        answerList.appendChild(answer4);
+
+    //contet for div
+
+        //div
+        // createQuestion.textContent =
+        //contents for h3
+        createQuestionH3.textContent = "Question # (x)" //+ questionNumber[index]; //add this part later and remove x
+        //contents for h4
+        createQuestionH4.textContent = "Question: "
+        //content for ol
+        //contents for li
+        answer1.textContent = "A. " //+ answers content. One will be correct and the other 3 incorrect.;
+        answer2.textContent = "B. " //+ answers content. One will be correct and the other 3 incorrect.;
+        answer3.textContent = "C. " //+ answers content. One will be correct and the other 3 incorrect.;
+        answer4.textContent = "D. " //+ answers content. One will be correct and the other 3 incorrect.;
+
+    //setAttribute for each
+
+        //div
+        //createQuestion.setAttribute();
+
+        //h3
+        createQuestionH3.setAttribute("style", "font-size: 250%; outline: 5px solid #7bf343; border-radius: 5px; margin: 1rem; padding: 0.5rem;");
+        //h4
+        createQuestionH4.setAttribute("style", "font-size: 150%; outline: 5px solid #7bf343; border-radius: 5px; margin-top: 1rem; margin-left: 1rem; margin-right: 1rem; margin-bottom: 3.5rem; padding: 0.5rem;");
+        //ol
+        // answerList.setAttribute();
+
+        //li
+        answer1.setAttribute("style", "background-color: #b6b8b9; color: blue; outline: 1px solid black; border-radius: 5px; margin: 1rem; padding: 1.5rem;");
+        answer2.setAttribute("style", "background-color: #b6b8b9; color: blue; outline: 1px solid black; border-radius: 5px; margin: 1rem; padding: 1.5rem;");
+        answer3.setAttribute("style", "background-color: #b6b8b9; color: blue; outline: 1px solid black; border-radius: 5px; margin: 1rem; padding: 1.5rem;");
+        answer4.setAttribute("style", "background-color: #b6b8b9; color: blue; outline: 1px solid black; border-radius: 5px; margin: 1rem; padding: 1.5rem;");
+        // answer2.setAttribute();
+        // answer3.setAttribute();
+        // answer4.setAttribute();
+
+
+
+
+
+
 //if you want to: think about making a rules page with a button to continue or to exit the quiz. once continue is pushed the quiz will begin.
 
 //create a timer for the page that will be triggered later.
